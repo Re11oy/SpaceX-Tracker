92f029d5e413e761c35001c0f43eb1097c07ee4d
Kirill <kirik.yes@gmail.com>
2019-10-16 23:00:02 +0300

    styled components with typescript

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import styled from 'styled-components/native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ScreenTitle from './src/Common/ScreenTitle';
+import { SafeAreaView } from 'react-native';
+import ScreenBackground from './src/Common/ScreenBackground';
 
 const StyledView = styled.View`
   background-color: papayawhip;
@@ -12,16 +15,31 @@ const StyledText = styled.Text`
   color: palevioletred;
 `;
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <StyledView>
+const Wrapper = styled(ScreenBackground)`
+  flex: 1;
+  padding: 40px 0 0 0;
+  margin: 100px;
+`;
+
+const ContentWrapper = styled(SafeAreaView)`
+  flex: 1;
+  justify-content: center;
+  background-color: aqua;
+`;
+
+export interface Props {}
+
+const HomeScreen: React.FC<Props> = () => {
+  return (
+    <Wrapper>
+      <ScreenTitle title={'Test'} />
+      <ContentWrapper>
         <StyledText>Open up App.tsx to start working on your app!!</StyledText>
         <Icon name="rocket" size={30} color="#900" />
-      </StyledView>
-    );
-  }
-}
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
 
 const AppNavigator = createStackNavigator({
   Home: {
---
+++ src/Common/ScreenBackground.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export interface Props {
+  style?: any;
+}
+const ScreenBackground: React.FC<Props> = ({ children, style }) => {
+  return (
+    <LinearGradient colors={['#fff', '#000']} start={{ x: 0.0, y: 0.25 }} end={{ x: 0.5, y: 1.0 }} style={style}>
+      {children}
+    </LinearGradient>
+  );
+};
+
+export default ScreenBackground;
